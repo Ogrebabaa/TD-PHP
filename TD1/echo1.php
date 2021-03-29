c2aa1adf16315d8c21543d34b38b7a8803dd3a2c
Valentin Moreau <valentin.m1999@gmail.com>
2021-03-29 14:24:24 +0200

    first commit

---
+++ TD1/echo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP i'm databyte</title>
+</head>
+<body>
+    <?php
+        $toto = "Yo les potes.";
+        echo $toto;
+    ?>
+</body>
+</html>
