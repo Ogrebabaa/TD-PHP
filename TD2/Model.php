4d05a6df33a86ef3cb3d341c96b794842a8d4095
Valentin Moreau <valentin.m1999@gmail.com>
2021-03-30 09:47:41 +0200

    gestion du mode debug

--- TD2/Model.php
+++ TD2/Model.php
@@ -14,7 +14,12 @@ Class Model {
             self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
-            echo $e->getMessage();
+            if (Conf::getDebug()) {
+                echo $e->getMessage();
+            } else {
+                echo "une erreur est survenue, <a href=\"\">retour à la page d'accueil </a>";
+            }
+            
             die();
         }
 
